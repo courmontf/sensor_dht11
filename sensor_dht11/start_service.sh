53c85198fe904d89ec43261a377200e0f0d8b62e
François Courmont <francois@courmont.net>
2024-01-17 18:58:17 +0100

    fix: script order and python path

--- sensor_dht11/start_service.sh
+++ sensor_dht11/start_service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-PYTHONPATH=/opt/sensor_dht11/venv/lib/python3.9/site-packages python3 -m uvicorn sensor_dht11:app --reload --host 0.0.0.0 --port 5000
+python3 -m uvicorn sensor_dht11:app --reload --host 0.0.0.0 --port 5000
