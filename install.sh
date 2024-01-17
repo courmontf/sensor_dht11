8751613a63d41a2cc10d12b30a49a9fe57ea2aad
François Courmont <francois@courmont.net>
2024-01-17 18:49:47 +0100

    fix: install script order

--- install.sh
+++ install.sh
@@ -16,12 +16,12 @@ fi
 $SUDO apt-get install libgpiod2
 git clone $GIT_REPOSITORY /tmp/sensor_dht11
 cd /tmp/sensor_dht11/sensor_dht11
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
 $SUDO chown -R root:root .
 $SUDO cp -r /tmp/sensor_dht11/sensor_dht11 $INSTALL_DIR
 cd $INSTALL_DIR
+$SUDO python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 $SUDO cp sensor_dht11.service /lib/systemd/system/sensor_dht11.service
 $SUDO chmod -R +x $INSTALL_DIR
 $SUDO useradd sensor_dht11 -G gpio -M -r
