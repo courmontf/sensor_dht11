bc8da7c9b740e263c5a4e71cefe5aa92813c6380
François Courmont <francois@courmont.net>
2024-01-17 18:18:34 +0100

    fix: bad path with an old template

--- sensor_dht11/sensor_dht11.py
+++ sensor_dht11/sensor_dht11.py
@@ -3,13 +3,15 @@ import board
 import adafruit_dht
 from fastapi import FastAPI
 
-app = FastAPI()
 
+app = FastAPI()
 sensor = adafruit_dht.DHT11(board.D23)
+
 
 @app.get("/sensor/dht11/state")
 def temperature():
     return {"temperature": sensor.temperature, "humidity": sensor.humidity}
+
 
 if __name__ == "__main__":
     app.run(debug=False)
